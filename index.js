074e9004d7dba6cb0acab5c92a3e52e720accb7a
Ondicho <albert.ondicho@moringaschool.com>
2022-06-09 07:58:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let total=0;
+let totalBatteries=batteryBatches.reduce(function(total,batteryBatches){
+    // console.log(batteryBatches)
+    total=total+batteryBatches
+    // console.log(total)
+    return total
+})
+console.log(total)
